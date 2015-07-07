95856b253816c74618c21af532a5c896eae1247d
JulianMoyano <JulianMoyano@users.noreply.github.com>
2015-07-07 23:06:11 +0200

    Create schema.js

---
+++ schema.js
@@ -0,0 +1,38 @@
+{
+    "types": {
+        "Archivos": {
+            "pluralLabel": "Archivos"
+        },
+        "Fondo": {
+            "pluralLabel": "Fondoss"
+        }
+    },
+    "properties": {
+        "imageURL": {
+            "valueType": "url"
+        },
+        "url": {
+            "valueType": "url"
+        },
+        "localidad": {
+            "valueType":              "item"
+        },
+		 "comarca": {
+            "valueType":              "item"
+        },
+        "term": {
+            "valueType":              "number"
+        },
+        "inDate": {
+            "valueType":              "date",
+            "label":                  "in"
+        },
+        "outDate": {
+            "valueType":              "date",
+            "label":                  "out"
+        },
+        "death": {
+            "valueType":              "date"
+        }
+    }
+}
